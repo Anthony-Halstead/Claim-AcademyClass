7fa235030eb76213edbf6b9b4c89caee757b647f
Anthony Halstead <anthony.dan.hal@gmail.com>
2023-04-03 11:54:03 -0500

    adding method for calculating area and circumference

--- WeekOne/Test.java
+++ WeekOne/Test.java
@@ -1,8 +1,23 @@
 package WeekOne;
 
+import java.util.Scanner;
+
 public class Test{
 public static void main(String[] args)
 {
   System.out.println("Hello World");
+
+  System.out.println("Type in a radius that is a decimal or whole number");
+  Scanner scanner = new Scanner(System.in);
+  double radius = scanner.nextDouble();
+  getAreaAndCircumference(radius);
+
+ 
 }
+public static void getAreaAndCircumference(double radius){
+   double circumference = 2*Math.PI*radius;
+   double area = Math.PI * Math.pow(radius, 2);
+
+    System.out.println("The Area is: "+area+" and the Circumference is: "+circumference);
+    }
 }
